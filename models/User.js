b0cfccbee0c2f545ea2cfce8957447e81d9ce59d
billvog <billvog04@gmail.com>
2021-01-20 14:02:46 +0200

    Updated Qr URL to home server

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { nanoid } = require('nanoid');
 const qrcode = require('qrcode');
+var url = require('url');
 
 const userSchema = new mongoose.Schema({
     _id: {
@@ -64,7 +65,7 @@ userSchema.virtual('avatarImagePath').get(function() {
 
 userSchema.virtual('qrImagePath').get(async function() {
     try {
-        return await qrcode.toDataURL(`https://perid.tk/pid/${this._id}`);
+        return await qrcode.toDataURL(`https://perid.ddns.net/pid/${this._id}`);
     }
     catch (error) {
         console.log(error);
